75faeb3afd685f3ec7d7c1a9e9cb0ada49bebbf8
Martin Goellnitz <martin@goellnitz.de>
2016-04-27 16:08:47 +0200

    Prepare decent environment for git bash

--- env.sh
+++ env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=/gradle-2.12/bin
+export JAVA_HOME=/c/Program\ Files/Java/jdk1.7.0_76
 export OPENSHIFT_MONGODB_DB_HOST=ds021671.mlab.com
 export OPENSHIFT_MONGODB_DB_PORT=21671
 export OPENSHIFT_APP_NAME=girlsday
